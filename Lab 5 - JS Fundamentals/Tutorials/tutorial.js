7afcfde1a1348ee7e4cf96c950d14a034e930902
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-03-01 15:42:10 +0300

    updaed

--- Lab 5 - JS Fundamentals/Tutorials/tutorial.js
+++ Lab 5 - JS Fundamentals/Tutorials/tutorial.js
@@ -49,7 +49,7 @@ const numbers = [1, 2, [3, 4], 5, 6, "Abdulhi", [1, 2, 3], person];
 numbers[0] = 1000;
 
 console.log(numbers);
-*/
+
 
 const flatNum = [1, [2, 3], 4, 5];
 const numbers = [1, 2, 3, 4, 5];
@@ -72,3 +72,47 @@ console.log("\nORIGINAL NOT AFFECTED [SLICE]", numbers);
 const spliced = numbers.splice(2, 1, 55, 66, 88, 99);
 console.log("\nSLICE", spliced);
 console.log("\nORIGINAL AFFECTED [SPLICED]", numbers);
+
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+// Where we want to reach
+
+// numbers.forEach((a) => console.log(a));
+// console.log(
+//   "Sum",
+//   numbers.reduce((a, b) => a + b)
+// );
+
+
+function add(a, b) {
+  return a + b;
+}
+
+const add2 = function (a, b) {
+  return a + b;
+};
+
+function display(v) {
+  console.log(v);
+}
+function display2(v) {
+  console.log("The value is ", v);
+}
+function addAndDisplay(a, b, dis) {
+  const result = a + b;
+  dis(result);
+}
+const a = 10;
+const c = 20;
+const d = 55;
+
+console.log(add(a, c));
+console.log(add2(a, 20));
+console.log("\nAfter calling the addAndDisplay");
+addAndDisplay(a, 20, display);
+addAndDisplay(a, 20, display2);
+*/
+
+function calculator ()
