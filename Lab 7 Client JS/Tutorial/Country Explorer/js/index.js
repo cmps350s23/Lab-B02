d8ed4310f4c0c6926ccf6655001667a5e4280e54
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-03-22 16:47:44 +0300

    final code

--- Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
@@ -20,11 +20,12 @@ async function handleRegionChange() {
         </option>
     `)
     countryDD.innerHTML = options
+    handleCountryChange()
 
 }
 
 async function handleCountryChange() {
-    // concatinate country url with the selected country
+    // concatenate country url with the selected country
     //get the data using the new url
     //parse it
     //console.log
@@ -36,7 +37,48 @@ async function handleCountryChange() {
 }
 
 function changeCountryToFacts(country) {
+    const currKey = Object.keys(country.currencies)[0]
+    const languages = Object.values(country.languages).join(' - ')
+
     return `
-        <h2> Facts About ${country.name.common} </h2>
+        <u><h2>  Facts About ${country.name.common} </h2></u>
+        <img src="${country.flags.png}" alt="${country.flags.alt}">
+        <br>
+        <table>
+            <tr>
+                <th>Official Name</th>
+                <td>${country.name.official}</td>
+            </tr>
+            <tr>
+                <th>Capital City</th>
+                <td>${country.capital}</td>
+            </tr>
+            <tr>
+                <th>Currencies</th>
+                <td>${country.currencies[currKey].name}</td>
+            </tr>
+            <tr>
+                <th>Population</th>
+                <td>${country.population}</td>
+            </tr>
+            <tr>
+                <th>Languages</th>
+                <td>${languages}</td>
+            </tr>
+
+        </table>
     `
 }
+
+
+// const person = {
+//     name: "Ali",
+//     age: 22
+// }
+
+// const keys = Object.keys(person)
+
+// console.log(person.name);
+// console.log(person['name']);
+// console.log(person[keys[0]]);
+// console.log(Object.values(person));
