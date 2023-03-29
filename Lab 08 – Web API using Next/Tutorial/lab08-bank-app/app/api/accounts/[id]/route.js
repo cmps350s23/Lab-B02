84869e61b0470e674bbf55ae56c95ab85dc2f2b0
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-03-29 15:15:15 +0300

    update the ds-extra nanoid and the route for api/accounts

--- Lab 08 – Web API using Next/Tutorial/lab08-bank-app/app/api/accounts/[id]/route.js
+++ Lab 08 – Web API using Next/Tutorial/lab08-bank-app/app/api/accounts/[id]/route.js
@@ -1,19 +1,24 @@
 export async function GET(request, { params }) {
     const { id } = params;
-    const accounts = [
-        {
-            "accountNo": 2002,
-            "acctType": "Saving",
-            "balance": 8000,
-            "minimumBalance": 1000
-        },
-        {
-            "accountNo": 1102,
-            "acctType": "Current",
-            "balance": 500,
-            "monthlyFee": 15
-        }
-    ]
+    // const accounts = [
+    //     {
+    //         "accountNo": 2002,
+    //         "acctType": "Saving",
+    //         "balance": 8000,
+    //         "minimumBalance": 1000
+    //     },
+    //     {
+    //         "accountNo": 1102,
+    //         "acctType": "Current",
+    //         "balance": 500,
+    //         "monthlyFee": 15
+    //     }
+    // ]
+
+    const accounts = await fs.readJSON('./data/accounts.json')
+    fs.writeJSON('./data/accounts.json', account)
+
+
     const account = accounts.find(account => account.accountNo === parseInt(id));
     return Response.json(account);
 }
