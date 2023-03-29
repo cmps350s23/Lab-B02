9df155998e417e531865e1a7cffd6c1f6fa70ac2
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-03-29 15:19:45 +0300

    updated

--- Lab 08 – Web API using Next/Tutorial/lab08-bank-app/app/api/accounts/route.js
+++ Lab 08 – Web API using Next/Tutorial/lab08-bank-app/app/api/accounts/route.js
@@ -1,19 +1,10 @@
+import AccountsRepo from "./accounts-repo"
+const repo = new AccountsRepo()
+
 export async function GET(request) {
     const { searchParams } = new URL(request.url)
     const type = searchParams.get('type')
-    const accounts = [
-        {
-            "accountNo": 2002,
-            "acctType": "Saving",
-            "balance": 8000,
-            "minimumBalance": 1000
-        },
-        {
-            "accountNo": 1102,
-            "acctType": "Current",
-            "balance": 500,
-            "monthlyFee": 15
-        }
-    ]
+    const accounts = await repo.getAccounts(type)
+
     return Response.json(accounts)
 }
