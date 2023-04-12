130d01d225fd71cd9d7cff5702cc45e72da64a3b
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-04-12 16:44:39 +0300

    updated

--- Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/Accounts.js
+++ Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/Accounts.js
@@ -1,12 +1,23 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import styles from '../page.module.css'
 import Account from './Account'
 
+
 export default function Accounts({ initialAccounts }) {
+
+    const [accounts, setAccounts] = useState(initialAccounts)
+
+    async function handleTypeChanged(e) {
+        const data = await fetch(`/api/accounts?type=${e.target.value}`)
+        // accounts = await data.json()
+        setAccounts(await data.json())
+        console.log(accounts);
+    }
     return (
         <div>
             <label htmlFor="acctType" className={styles.label}>Account Type</label>
-            <select id="acctType" className={styles.dropdown}>
+            <select id="acctType" className={styles.dropdown} onChange={e => handleTypeChanged(e)}>
                 <option value="All">All</option>
                 <option value="Saving">Saving</option>
                 <option value="Current">Current</option>
@@ -22,7 +33,7 @@ export default function Accounts({ initialAccounts }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {initialAccounts.map(account => <Account account={account}></Account>)}
+                    {accounts.map(account => <Account account={account}></Account>)}
                 </tbody>
             </table>
         </div>
