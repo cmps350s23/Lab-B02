bd0098c6c58a6028c78e09cab3b7d7f5808c03f5
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-05-17 03:03:02 +0300

    lab 13

---
+++ lab13-1-bank-app -Updated/app/api/users/user-repo.js
@@ -0,0 +1,38 @@
+import prisma from "@/app/libs/prisma";
+
+export const createUser = async (user) => {
+    try {
+        // before adding the user we need to hash the password
+        // add the bcrypt hash here
+
+        const newUser = await prisma.user.create({
+            data: user
+        })
+
+        delete newUser.password
+        return newUser
+    } catch (error) {
+        return { error: error.message }
+    }
+}
+
+export const getUser = async (email, password) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email: email }
+        })
+        if (!user) return { error: "User does not exist" }
+
+        //add the bcrypt compare here
+        const isMath = await compare(password, user.password)
+
+        if (!isMath) return { error: "Password is incorrect" }
+
+        // delete the password from the user object before returning it
+
+        delete user.password
+        return user
+    } catch (error) {
+        return { error: error.message }
+    }
+}
